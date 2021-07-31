fafb1e19706027bafb9143d97dbae6add2c32772
Ivan Dernovyi <idernovoy@gmail.com>
2021-07-31 19:36:05 +0300

    task-01
    Виконано завдання №1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,19 @@
-const 
+const listCategories = document.querySelectorAll('ul#categories > li.item');
+
+function elementProcessing(element) {
+    const childElm = element.children;
+
+    for (let el of childElm) {
+        switch (el.tagName) {
+            case "H2":
+                console.log(`Категорія: ${el.textContent}`);
+                break;
+            case "UL":
+                console.log(`Кількість елементів: ${el.children.length}`);
+                break;
+        };
+    };
+};
+
+console.log(`У списку ${listCategories.length} категорії.`);
+listCategories.forEach(categorie => elementProcessing(categorie));
