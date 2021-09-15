42a523db19b346d972ef2dd60d1060e48f0c1673
Ivan Dernovyi <idernovoy@gmail.com>
2021-09-15 22:27:02 +0300

    project
    Change task #6

--- js/task-06.js
+++ js/task-06.js
@@ -5,16 +5,24 @@ const input = document.querySelector("input#validation-input");
 const handlerBlur = (event) => {
     const elm = event.target;
     if (elm.value.length === Number(elm.dataset.length)) {
-        if (elm.classList.contains(INVALID_CLASS_NAME))
-            elm.classList.remove(INVALID_CLASS_NAME);
-        if (!elm.classList.contains(VALID_CLASS_NAME))
+        if (elm.classList.length != 1)
+            elm.classList.replace(INVALID_CLASS_NAME, VALID_CLASS_NAME);
+        else
             elm.classList.add(VALID_CLASS_NAME);
+        // if (elm.classList.contains(INVALID_CLASS_NAME))
+        //     elm.classList.remove(INVALID_CLASS_NAME);
+        // if (!elm.classList.contains(VALID_CLASS_NAME))
+        //     elm.classList.add(VALID_CLASS_NAME);
     }
     else {
-        if (elm.classList.contains(VALID_CLASS_NAME))
-            elm.classList.remove(VALID_CLASS_NAME);
-        if (!elm.classList.contains(INVALID_CLASS_NAME))
+        if (elm.classList.length != 1)
+            elm.classList.replace(VALID_CLASS_NAME, INVALID_CLASS_NAME);
+        else
             elm.classList.add(INVALID_CLASS_NAME);
+        // if (elm.classList.contains(VALID_CLASS_NAME))
+        //     elm.classList.remove(VALID_CLASS_NAME);
+        // if (!elm.classList.contains(INVALID_CLASS_NAME))
+        //     elm.classList.add(INVALID_CLASS_NAME);
     }
 };
 
