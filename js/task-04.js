0373df2657549329f0a9177fd5d04951d4a2683a
Ivan Dernovyi <idernovoy@gmail.com>
2021-09-19 21:28:31 +0300

    Update task-04.js
    Refactoring task-04

--- js/task-04.js
+++ js/task-04.js
@@ -1,27 +1,29 @@
 let counterValue = 0;
 
 const span = document.querySelector("span#value");
-const counter = document.querySelector("div#counter");
+const buttonDec = document.querySelector('[data-action="decrement"]');
+const buttonInc = document.querySelector('[data-action="increment"]');
 
-function decrement() {
-    counterValue--;
-    span.textContent = counterValue;
-};
+// function decrement() {
+//     counterValue--;
+//     span.textContent = counterValue;
+// };
 
-function increment() {
-    counterValue++;
-    span.textContent = counterValue;
-};
+// function increment() {
+//     counterValue++;
+//     span.textContent = counterValue;
+// };
 
-const handleClick = (event) => {
-    if (event.target.nodeName !== "BUTTON") return;
-    switch (event.target.dataset.action) {
-        case 'decrement':
-            decrement();
-            break;
-        case 'increment':
-            increment();
-    }
-};
+// const handleClick = (event) => {
+//     if (event.target.nodeName !== "BUTTON") return;
+//     switch (event.target.dataset.action) {
+//         case 'decrement':
+//             decrement();
+//             break;
+//         case 'increment':
+//             increment();
+//     }
+// };
 
-counter.addEventListener("click", handleClick);
+buttonDec.addEventListener("click", () => span.textContent = --counterValue);
+buttonInc.addEventListener("click", () => span.textContent = ++counterValue);
