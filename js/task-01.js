4f384992c53d498149a049c3da1af7e1b7e7dd88
Ivan Dernovyi <idernovoy@gmail.com>
2021-09-19 21:16:58 +0300

    Update task-01.js
    Refactoring Task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,22 @@
 const listCategories = document.querySelectorAll('ul#categories > li.item');
 
-function elementProcessing(elements) {
+// function elementProcessing(elements) {
 
-    for (let el of elements) {
-        switch (el.tagName.toUpperCase()) {
-            case "H2":
-                console.log(`Категорія: ${el.textContent}`);
-                break;
-            case "UL":
-                console.log(`Кількість елементів: ${el.children.length}`);
-                break;
-        };
-    };
-};
+//     for (let el of elements) {
+//         switch (el.tagName.toUpperCase()) {
+//             case "H2":
+//                 console.log(`Категорія: ${el.textContent}`);
+//                 break;
+//             case "UL":
+//                 console.log(`Кількість елементів: ${el.children.length}`);
+//                 break;
+//         };
+//     };
+// };
 
 console.log(`У списку ${listCategories.length} категорії.`);
-listCategories.forEach(categorie => elementProcessing(categorie.children));
+listCategories.forEach(categorie => {
+    console.log(`Категорія: ${categorie.firstElementChild.textContent}`);
+    console.log(`Кількість елементів: ${categorie.lastElementChild.children.length}`);
+    // elementProcessing(categorie.children)
+});
